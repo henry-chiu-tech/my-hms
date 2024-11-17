7ae880742fa9e18fd31476ea12ed67080df222af
henry <henry.chiu.tech@gmail.com>
2024-11-17 23:10:07 +1100

    removed some typescript rules to build and remove unused router in Logo.tsx

--- app/components/navbar/logo.tsx
+++ app/components/navbar/logo.tsx
@@ -1,10 +1,8 @@
 'use client';
 
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 
 const Logo = () => {
-    const router = useRouter();
 
     return (
         <Image
